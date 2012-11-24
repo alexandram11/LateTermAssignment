70dc90b521f523d3f0b59339283dfdf8f0af1fd7
alexandram11 <alexandram11@ru.is>
2012-11-24 14:01:54 +0000

    commented Gui.java

--- src/Presentation/TicTacGUI.java
+++ src/Presentation/TicTacGUI.java
@@ -8,14 +8,18 @@ import java.awt.event.ActionEvent;
 
 /**
  * This represents the GUI for the TicTac game.
- * //TODO vantar að commenta.
+ *
  */
 public class TicTacGUI extends AbsTicTacGameGUI {
     public TicTacGUI(TicTacInterface Game) {
         super();
         gameLogic = Game;
     }
-    //TODO Þetta er til að taka á móti smellum úr viðmóti..
+
+    /**
+     * This method is to get the actions (clicks) from the user
+     * @param e is the click coming in from user
+     */
     public void actionPerformed(ActionEvent e) {
         if (e.getActionCommand().equals("NEW GAME")) {
             lower.startNewGame();
@@ -41,6 +45,9 @@ public class TicTacGUI extends AbsTicTacGameGUI {
         }
     }
 
+    /**
+     * this method cleans the board after a game finishes
+     */
     private void cleanUp() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -49,6 +56,9 @@ public class TicTacGUI extends AbsTicTacGameGUI {
         }
     }
 
+    /**
+     * new frame created and displayed
+     */
     private static void createAndShowGUI() {
         //Create and set up the window.
         JFrame frame = new JFrame("ButtonTest");
